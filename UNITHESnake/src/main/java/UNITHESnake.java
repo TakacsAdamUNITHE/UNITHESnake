71a8f1b5acfd8cb3c068fbe9b81dae9ee530648a
Takacs Adam <159785600+TakacsAdamUNITHE@users.noreply.github.com>
2024-04-03 14:59:56 +0200

    mozgas alapok
    a kigyo mozgasahoz szukseges alapok kiepitese

--- UNITHESnake/src/main/java/UNITHESnake.java
+++ UNITHESnake/src/main/java/UNITHESnake.java
@@ -10,23 +10,37 @@
 public class UNITHESnake {
     //a palya adatainak szamontartasahoz szukseges dolgok
     //ertek magyarazat:
-    //0: a mezo ures
-    //1: a kigyo teste
-    //2: bogyo
-    int [] [] snakemap;
-    int x,y;
+    //false: a mezo ures
+    //true: bogyo
+    //a snakecord tomb a kigyo helyzetenek a szamontartasara szolgal
+    //a mozgas szimulalasa erdekeben a tomb elso elemet elvesszuk es a vegera egy ujat rakunkbe
+    boolean [] [] snakemap;
+    int [][] snakecord;
+    int x,y,area,v;
     
     //a tomb alap helyzetbe allitasa
     UNITHESnake (int x, int y){
         this.x=x;
         this.y=y;
-        snakemap = new int [x] [y];
+        snakemap = new boolean [x] [y];
         for (int i=0;i<x;i++){
             for (int j = 0;j<y;j++){
-                snakemap[i][j]=0;
+                snakemap[i][j]=false;
             }
                 
         }
+
+        //a kigyo kordonatainak tarolasa
+        //area: a palya teruletenek kiszamitasa
+        area=x*y;
+        v=1;
+        snakecord = new int [area][v];
+        for (int i=0;i<area;i++){
+            for (int j=0;j<v;j++){
+               snakecord[i][j] = 0;
+            }
+        }
+        
     }
     
     public int getx(){
