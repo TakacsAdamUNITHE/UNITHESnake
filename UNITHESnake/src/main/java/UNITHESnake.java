1d320a80e08ba6fb1cf7a3728a5623d092b1c1fc
Takacs Adam <159785600+TakacsAdamUNITHE@users.noreply.github.com>
2024-04-29 15:32:39 +0200

    update 2
    a GUI is maga a kod kulon lete gondokat okozot ezert egy uj probalkozasi modszer done: start screen TODO: a jatek es egy game over helyzet

--- UNITHESnake/src/main/java/UNITHESnake.java
+++ UNITHESnake/src/main/java/UNITHESnake.java
@@ -7,60 +7,151 @@
  *
  * @author Göblyös Bence, Takács Ádám
  */
-public class UNITHESnake {
-    //a palya adatainak szamontartasahoz szukseges dolgok
-    //ertek magyarazat:
-    //false: a mezo ures
-    //true: bogyo
-    //a snakecord tomb a kigyo helyzetenek a szamontartasara szolgal
-    //a mozgas szimulalasa erdekeben a tomb elso elemet elvesszuk es a vegera egy ujat rakunkbe
-    //boolean [] [] snakemap;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class UNITHESnake extends JPanel implements KeyListener, ActionListener {
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 400;//a palya meretei
+    private static final int EGYSEG = 20;
+    private final int [] x = new int[EGYSEG];
+    private final int [] y = new int[EGYSEG];
+    private boolean running = false; //a jatak helyzetenek a szamontartasa
+    private boolean startKepernyo = true; //a kezdo kepernyo TODO: kesobbi game over kepernyot ugyan így
+    private Timer timer; //lepeshez szukseges idozito (javax swing)
+    private JButton startButton; //start gomb
+    private char irany = 'R'; //mozgasi irany
+    private int bogyoX;
+    private int bogyoY; //bogyo kordinatak
+    private int hossz = 3; // a kezdo hossz
     
-    //a korabbi valtoztatasok miat szerintem nem igazan lesz szukseg maga a palya szamon tartasara
-    //ezert csak a pont x es y kordinatajat fogom szamon tartani
-    //a pont szerzets meg ugy lehetlesz megoldani hogy elenorizuk hogy a pont valtozoban levo elemek
-    //benne vannak-e a snakecord-ban
-    
-    int [] pont;
-    int [][] snakecord;
-    int x,y,area,v;
-    
-    //a tomb alap helyzetbe allitasa
-    UNITHESnake (int x, int y){
-        this.x=x;
-        this.y=y;
-        //snakemap = new boolean [x] [y];
-        //for (int i=0;i<x;i++){
-        //    for (int j = 0;j<y;j++){
-        //        snakemap[i][j]=false;
-        //    }
-        //        
-        //}
+    public UNITHESnake(){
+        setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        setBackground(Color.BLACK);
+        setFocusable(true);
+        addKeyListener(this);
+        startButton = new JButton("Start");
+        startButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                startKepernyo = false;
+                startButton.setVisible(false);
+                startGame();
+            }
+        });
+        add(startButton);
+    }
 
-        pont = new int [2];
-        pont[0]=4; //x kordinata
-        pont[1]=3;   //y kordinata
+    //start-hoz szukseges dolgok elkeszitese
+    public void startGame(){
+        newBogyo();
+        running = true;
+        timer = new Timer(100,this);
+        timer.start();
         
-        
-        //a kigyo kordonatainak tarolasa
-        //area: a palya teruletenek kiszamitasa
-        area=x*y;
-        v=1;
-        snakecord = new int [area][v];
-        for (int i=0;i<area;i++){
-            for (int j=0;j<v;j++){
-               snakecord[i][j] = 0;
+    }
+    
+    //start screen
+    public void startScreen(Graphics g) {
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial", Font.BOLD,30));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("UNI THE Snake Game",(WIDTH - metrics.stringWidth("Snake Game")) / 2, HEIGHT / 2 - 50);
+        startButton.setBounds((WIDTH - 100) / 2, HEIGHT / 2, 100, 40);
+    }
+    
+    //a kepernyo valtasi vezerlo
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (startKepernyo){
+            drawStart(g);
+        }
+    }
+    
+    public void drawStart(Graphics g){
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 30));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Snake Game", (WIDTH - metrics.stringWidth("Snake Game")) / 2, HEIGHT / 2 - 50);
+        startButton.setBounds((WIDTH - 100) / 2, HEIGHT / 2, 100, 40);
+    }
+    
+    
+    //---a jatek---
+    
+    //bogyo elhelyezese
+    public void newBogyo() {
+        bogyoX = (int) (Math.random() * (WIDTH / EGYSEG)) * EGYSEG;
+        bogyoY = (int) (Math.random() * (HEIGHT / EGYSEG)) * EGYSEG;
+    }
+    
+    
+    public void mozgas(){
+        for (int i = hossz; i>0; i--){
+            x[i]=x[i-1];
+            y[i]=y[i-1];
+        }
+        switch (irany) {
+            case 'U' -> y[0]=y[0] - EGYSEG;
+            case 'D' -> y[0]=y[0] + EGYSEG;
+            case 'L' -> x[0]=x[0] - EGYSEG;
+            case 'R' -> x[0]=x[0] + EGYSEG;
+        }
+    }
+    
+    
+    //--- override-ok ---
+    
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if (running){
+            mozgas();
+            
+        }
+        repaint();
+    }
+    @Override
+    public void keyPressed(KeyEvent e){
+        switch (e.getKeyCode()){
+            //bal
+            case KeyEvent.VK_LEFT ->{
+                if (irany != 'R'){
+                    irany = 'L';
+                } 
+            }
+            //job
+            case KeyEvent.VK_RIGHT ->{
+                if (irany != 'L'){
+                    irany = 'R';
+                } 
+            }
+            //fel
+            case KeyEvent.VK_UP ->{
+                if (irany != 'D'){
+                    irany = 'U';
+                } 
+            }
+            //le
+            case KeyEvent.VK_DOWN ->{
+                if (irany != 'U'){
+                    irany = 'D';
+                } 
             }
         }
-        
+    }
+    @Override
+    public void keyReleased(KeyEvent e){
+    }
+    @Override
+    public void keyTyped(KeyEvent e){
     }
     
-    public int getx(){
-        return this.x;
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Snake Game");
+        UNITHESnake game = new UNITHESnake();
+        frame.add(game);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
-    
-    public int gety(){
-        return this.y;
-    }
-    
 }
