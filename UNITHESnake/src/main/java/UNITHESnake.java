c4f9b90d98a9895827756cad70d28f02466e07f0
Takacs Adam <159785600+TakacsAdamUNITHE@users.noreply.github.com>
2024-05-01 11:34:26 +0200

    update 4
    a jatek nagyreszben mukodik TODO: egy az adatbazishoz szukseges mezo ahol a jateks betud irni egy nevet a pontszam elmentese erdekebe

--- UNITHESnake/src/main/java/UNITHESnake.java
+++ UNITHESnake/src/main/java/UNITHESnake.java
@@ -20,6 +20,7 @@ public class UNITHESnake extends JPanel implements KeyListener, ActionListener {
     private boolean startKepernyo = true; //a kezdo kepernyo TODO: kesobbi game over kepernyot ugyan így
     private Timer timer; //lepeshez szukseges idozito (javax swing)
     private JButton startButton; //start gomb
+    private JButton restartButton; //jatek ujra inditasa
     private char irany = 'R'; //mozgasi irany
     private int bogyoX;
     private int bogyoY; //bogyo kordinatak
@@ -40,6 +41,21 @@ public class UNITHESnake extends JPanel implements KeyListener, ActionListener {
             }
         });
         add(startButton);
+        restartButton = new JButton("Retry");
+        restartButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                for (int i=0;i<=0;i++){
+                    x[i]=0;
+                    y[i]=0;
+                }    
+                hossz=3;
+                pontok=0;
+                irany = 'R';
+                running = true;
+                restartButton.setVisible(false);
+                startGame();
+            }
+        });
     }
 
     //start-hoz szukseges dolgok elkeszitese
@@ -53,18 +69,21 @@ public class UNITHESnake extends JPanel implements KeyListener, ActionListener {
     
     //start screen
     public void startScreen(Graphics g) {
+        //a jatek nevenek kirajzolasa
         g.setColor(Color.white);
         g.setFont(new Font("Arial", Font.BOLD,30));
         FontMetrics metrics = getFontMetrics(g.getFont());
-        g.drawString("UNI THE Snake Game",(WIDTH - metrics.stringWidth("Snake Game")) / 2, HEIGHT / 2 - 50);
+        g.drawString("UNITHE Snake",(WIDTH - metrics.stringWidth("UNITHE Snake")) / 2, HEIGHT / 2 - 50);
+        //start gomb
         startButton.setBounds((WIDTH - 100) / 2, HEIGHT / 2, 100, 40);
     }
     
     //maga a jatek
     public void gameScreen (Graphics g){
         g.setColor(Color.RED);
+        //bogyo megrajzolasa
         g.fillOval(bogyoX,bogyoY,EGYSEG,EGYSEG);
-        
+        //kigyo rajzolasa
         for (int i = 0;i<hossz;i++){
             if (i == 0) {
                 g.setColor(Color.green);
@@ -73,30 +92,45 @@ public class UNITHESnake extends JPanel implements KeyListener, ActionListener {
             }
             g.fillRect(x[i], y[i], EGYSEG, EGYSEG);
         }
+        //pontok kirajzolasa
         g.setColor(Color.red);
         g.setFont(new Font("Arial", Font.BOLD, 25));
         FontMetrics metrics = getFontMetrics(g.getFont());
         g.drawString("Score: " + pontok, (WIDTH - metrics.stringWidth("Score: " + pontok)) / 2, g.getFont().getSize());
+        
+        
     }
     
-    
+    //game over kepernyo
+    public void gameOver(Graphics g){
+        //game over szoveg kirajzolasa
+        g.setColor(Color.red);
+        g.setFont(new Font("Arial", Font.BOLD, 40));
+        FontMetrics metrics1 = getFontMetrics(g.getFont());
+        g.drawString("Game Over", (WIDTH - metrics1.stringWidth("Game Over")) / 2, HEIGHT / 2);
+        //vegso pontszam kirajzolasa
+        g.setColor(Color.red);
+        g.setFont(new Font("Arial", Font.BOLD, 25));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Score: " + pontok, (WIDTH - metrics.stringWidth("Score: " + pontok)) / 2, g.getFont().getSize());
+        add(restartButton);
+        restartButton.setBounds((WIDTH - 100) / 2, (HEIGHT+50) / 2, 100, 40);
+        restartButton.setVisible(true);
+    }
     
     //a kepernyo valtasi vezerlo
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         if (startKepernyo){
-            drawStart(g);
+            startScreen(g);
+        }
+        else if (running){
+            gameScreen(g);
+        }
+        else {
+            gameOver(g);
         }
     }
-    
-    public void drawStart(Graphics g){
-        g.setColor(Color.WHITE);
-        g.setFont(new Font("Arial", Font.BOLD, 30));
-        FontMetrics metrics = getFontMetrics(g.getFont());
-        g.drawString("Snake Game", (WIDTH - metrics.stringWidth("Snake Game")) / 2, HEIGHT / 2 - 50);
-        startButton.setBounds((WIDTH - 100) / 2, HEIGHT / 2, 100, 40);
-    }
-    
     
     //---a jatek---
     
@@ -106,7 +140,7 @@ public class UNITHESnake extends JPanel implements KeyListener, ActionListener {
         bogyoY = (int) (Math.random() * (HEIGHT / EGYSEG)) * EGYSEG;
     }
     
-    
+    //kigyo mozgasa
     public void mozgas(){
         for (int i = hossz; i>0; i--){
             x[i]=x[i-1];
@@ -119,7 +153,32 @@ public class UNITHESnake extends JPanel implements KeyListener, ActionListener {
             case 'R' -> x[0]=x[0] + EGYSEG;
         }
     }
-    
+    //bogyo elenzorzese
+    public void bogyoCheck() {
+        if (x[0]==bogyoX && y[0]==bogyoY){
+            hossz++;
+            pontok++;
+            newBogyo();
+        }
+    }
+    //game over helyzetek figyelese
+    public void colisionCheck(){
+        //testbe utkozes
+        for (int i=hossz;i>0;i--){
+            if (x[0]==x[i]&&y[0]==y[i])
+                running=false;  
+        }
+        //falakba valo utkozes
+        if (x[0]<0 || x[0]>=WIDTH){
+            running = false;
+        }
+        if (y[0]<0 || y[0]>=HEIGHT){
+            running = false;
+        }
+        if (!running){
+            timer.stop();
+        }
+    }
     
     //--- override-ok ---
     
@@ -127,7 +186,8 @@ public class UNITHESnake extends JPanel implements KeyListener, ActionListener {
     public void actionPerformed(ActionEvent e){
         if (running){
             mozgas();
-            
+            colisionCheck();
+            bogyoCheck();
         }
         repaint();
     }
