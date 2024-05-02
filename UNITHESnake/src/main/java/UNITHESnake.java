55db468974ba458e8fa3b4bf289dc9d0fb1893de
goblyosbence0725 <115233975+goblyosbence0725@users.noreply.github.com>
2024-05-02 17:02:42 +0200

    Méretezés tiltása

--- UNITHESnake/src/main/java/UNITHESnake.java
+++ UNITHESnake/src/main/java/UNITHESnake.java
@@ -234,6 +234,7 @@ public class UNITHESnake extends JPanel implements KeyListener, ActionListener {
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
         frame.setVisible(true);
     }
 }
